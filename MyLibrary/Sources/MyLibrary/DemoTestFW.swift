2e891284357f1dbc5481925b7d1116d6ae55d247
annt <annt598@viettel.com.vn>
2020-12-10 08:36:36 +0700

    Test

---
+++ MyLibrary/Sources/MyLibrary/DemoTestFW.swift
@@ -0,0 +1,16 @@
+//
+//  DemoTestFW.swift
+//  DemoFWEnd
+//
+//  Created by annt on 12/10/20.
+//  Copyright © 2020 annt. All rights reserved.
+//
+
+import Foundation
+public class DemoTestFW {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
